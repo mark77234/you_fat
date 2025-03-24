cfcdb337997e39caf53f9cd7acb588632ec0d0ee
Trudy2645 <162314061+Trudy2645@users.noreply.github.com>
2025-03-24 20:43:38 +0900

    첫 커밋

---
+++ RudyMark/RudyMark/RudyMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  RudyMarkApp.swift
+//  RudyMark
+//
+//  Created by 트루디 on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RudyMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RudyMark/RudyMarkTests/RudyMarkTests.swift
@@ -0,0 +1,17 @@
+//
+//  RudyMarkTests.swift
+//  RudyMarkTests
+//
+//  Created by 트루디 on 3/24/25.
+//
+
+import Testing
+@testable import RudyMark
+
+struct RudyMarkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
