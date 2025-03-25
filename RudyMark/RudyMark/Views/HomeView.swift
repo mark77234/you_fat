a7103f2dcd282ef2e074c22d4b88bdc04b21b4f7
mark <mark77234@naver.com>
2025-03-25 11:36:26 +0900

    feat: 폴더 구조 및 gitignore추가

---
+++ RudyMark/RudyMark/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  RudyMark
+//
+//  Created by 이병찬 on 3/25/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack{
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
